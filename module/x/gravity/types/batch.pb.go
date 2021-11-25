993fa1fed1c2e827251857ba11ae417d3dc64182
dzmitryhil <dzmitryhil@gmail.com>
2021-11-25 12:13:53 +0300

    add erc20 to denom permanent swap as a parameter to genesis. Integrate with attestation handler to swap erc20->denon once the proper erc20 is received.

--- module/x/gravity/types/batch.pb.go
+++ module/x/gravity/types/batch.pb.go
@@ -807,10 +807,7 @@ func (m *OutgoingTxBatch) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthBatch
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthBatch
 			}
 			if (iNdEx + skippy) > l {
@@ -1015,10 +1012,7 @@ func (m *OutgoingTransferTx) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthBatch
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthBatch
 			}
 			if (iNdEx + skippy) > l {
@@ -1293,10 +1287,7 @@ func (m *OutgoingLogicCall) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthBatch
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthBatch
 			}
 			if (iNdEx + skippy) > l {
