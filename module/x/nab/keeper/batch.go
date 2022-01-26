6e80dfbabdbcdccb5c18c47ac5186bba3f47fe03
dzmitryhil <dzmitryhil@gmail.com>
2022-01-26 07:49:32 +0300

    const OutgoingTxBatchSize = 20

--- module/x/nab/keeper/batch.go
+++ module/x/nab/keeper/batch.go
@@ -11,7 +11,7 @@ import (
 	"github.com/onomyprotocol/near-aurora-bridge/module/x/nab/types"
 )
 
-const OutgoingTxBatchSize = 20
+const OutgoingTxBatchSize = 40
 
 // BuildOutgoingTXBatch starts the following process chain:
 // - find bridged denominator for given voucher type
